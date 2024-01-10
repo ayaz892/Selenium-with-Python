e9f69d21f9e96a91cf55aa0f3a723411312f8db8
Muhammad Ayaz Hasan <ayazhasan97@gmail.com>
2024-01-10 14:57:17 +0500

    Update basic1.py

--- basic1.py
+++ basic1.py
@@ -11,6 +11,8 @@ def selenium_textbox():
     first_name=driver.find_element(By.ID,"userName")       #search field by ID which is ="userName".Using inspect you can easily find lemebt by id or class
     first_name.send_keys("Jhon Cena")                           #send value "jhon cena"in that input field
     time.sleep(8)                                                 # after 8 sec automatically exit
+    first_name.clear()                                               #clear the field
+    time.sleep(8) 
 
 if __name__== '__main__':
     selenium_textbox()
